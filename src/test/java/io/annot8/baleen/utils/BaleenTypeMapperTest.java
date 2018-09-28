c172057964cd429edd5b373ae6fd7fb4e21740c7
Chris Flatley <chris@committed.io>
2018-09-28 10:31:53 +0100

    Add simple unit test to test coverage

---
+++ src/test/java/io/annot8/baleen/utils/BaleenTypeMapperTest.java
@@ -0,0 +1,20 @@
+package io.annot8.baleen.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.jcas.JCas;
+import org.junit.jupiter.api.Test;
+import uk.gov.dstl.baleen.types.common.Person;
+import uk.gov.dstl.baleen.uima.testing.JCasSingleton;
+
+class BaleenTypeMapperTest {
+
+  @Test
+  void fromBaleenToAnnot8() throws UIMAException {
+    BaleenTypeMapper mapper = new BaleenTypeMapper();
+    JCas jCas = JCasSingleton.getJCasInstance();
+    Person p = new Person(jCas);
+    assertThat(mapper.fromBaleenToAnnot8(p).get()).isEqualTo("baleen.entity.person");
+  }
+}
